abc8bd7fa2f94367064135cadeb04e7dc489c70b
didierganthier <didierganthierperan@gmail.com>
2020-09-02 12:29:58 -0400

    basic design in login_register_page.dart

--- lib/login_register_page.dart
+++ lib/login_register_page.dart
@@ -4,14 +4,18 @@ class LoginRegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
         title: Text("Flutter Blog App"),
       ),
-      body: Container(
-        margin: EdgeInsets.all(15.0),
-        child: Form(
-          child: Column(
-            children: [],
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.all(15.0),
+          child: Form(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: createInputs() + createButtons(),
+            ),
           ),
         ),
       ),
@@ -19,10 +23,34 @@ class LoginRegisterPage extends StatelessWidget {
   }
 }
 
+List<Widget> createInputs() {
+  return [
+    SizedBox(
+      height: 10.0,
+    ),
+    logo(),
+    SizedBox(
+      height: 20.0,
+    ),
+    TextFormField(
+      decoration: InputDecoration(labelText: 'Email', labelStyle: TextStyle(color: Colors.pink),border: OutlineInputBorder(borderSide: BorderSide(color: Colors.pink))),
+      autofocus: true,
+    ),
+    SizedBox(
+      height: 10.0,
+    ),
+    TextFormField(
+      decoration: InputDecoration(labelText: 'Password', labelStyle: TextStyle(color: Colors.pink),border: OutlineInputBorder(borderSide: BorderSide(color: Colors.pink))),
+    ),
+    SizedBox(
+      height: 10.0,
+    ),
+  ];
+}
 
-
-Widget logo(){
+Widget logo() {
   return Hero(
+    tag: 'hero',
     child: CircleAvatar(
       backgroundColor: Colors.transparent,
       radius: 110.0,
@@ -31,3 +59,24 @@ Widget logo(){
   );
 }
 
+List<Widget> createButtons() {
+  return [
+    RaisedButton(
+      onPressed: () {},
+      child: Text(
+        'Login',
+        style: TextStyle(fontSize: 20.0),
+      ),
+      textColor: Colors.white,
+      color: Colors.pink,
+    ),
+    FlatButton(
+      onPressed: () {},
+      child: Text(
+        "Don't have an account? Create Account",
+        style: TextStyle(fontSize: 15.0),
+      ),
+      textColor: Colors.white,
+    )
+  ];
+}
